a31fff336102af36dbf6312abd0918340c2e4700
Taylor <tbnelson01@gmail.com>
2022-11-03 20:40:43 -0400

    all descriptions showing

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import { bookInfo } from './bookinfo';
+import { useState } from 'react'
 
-console.log(bookInfo.length)
+
+
 
 function App() {
+  const [expanded, setExpanded] = useState(false)
+  const handleClick = (event) => {
+    setExpanded(!expanded)
+  }
+
   const bookList = bookInfo.map((book) => {
     return (
-      <div>
-        <h2>Title: {book.title}</h2>
-        <h4>Author: {book.author}</h4>
-        <p>Description: {book.shortDescription}</p>
-        <img src={book.coverImageUrl}></img>
-      </div>
+      <section id="books">
+        <div>
+          <h2>{book.title}</h2>
+          <h4>{book.author}</h4>
+          <p>{book.shortDescription}</p>
+          <button onClick={handleClick}>{expanded ? 'Less' : 'More'} information</button>
+          {expanded && (
+            <div>{book.detailedDescription}</div>
+          )}
+        </div>
+        <img className="cover" src={book.coverImageUrl}></img>
+      </section>
     )
   })
 
-  return (<div>{bookList}</div>)
+  return <div>
+    {bookList}
+  </div>
 }
 
 export default App;
